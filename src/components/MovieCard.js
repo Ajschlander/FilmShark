a4259f9663af244baf89f47ff2c3840d8b7c946b
Ajschlander <Aschlander21@gmail.com>
2020-02-03 17:29:51 -0600

    Sort buttons now work

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,14 +1,10 @@
-import React, {useState} from "react";
-import "../styles/MovieCard.css";
-import axios from "axios";
-
-let API_KEY = "6939281b4b2fc9bd592e14dec01248d5";
+import React from "react";
 
 const MovieCard = (props) => {
 
     return (
         <div>
-            <h5>{props.title}</h5>
+            <h4>{props.title}</h4>
         </div>
     )
 }
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,14 +1,16 @@
 import React, {useState, useEffect} from "react";
+import * as firebase from "firebase";
+import app from "../firebase";
 import { makeStyles } from '@material-ui/core/styles';
 import { Grid, Link, Typography } from "@material-ui/core";
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
-import * as firebase from "firebase";
-import app from "../firebase";
 import TextField from '@material-ui/core/TextField';
 import axios from "axios";
 import useDebounce from "../hooks/useDebounce";
 import logo from "../images/logo transparent.png";
 import "../styles/Navbar.css";
+import "../styles/Main.css"
+import MovieCard from "../components/MovieCard";
 
 let API_KEY = "6939281b4b2fc9bd592e14dec01248d5";
 
@@ -65,21 +67,21 @@ const Home = () => {
             [loaded, setLoaded] = useState(true),
             [query, setQuery]   = useState("Dark");
 
-    let debouncedQuery = useDebounce(query, 500);
+    // let debouncedQuery = useDebounce(query, 500);
 
-    useEffect(() => {
-    // Make sure we have a value (user has entered something in input)
-        if (debouncedQuery) {
-            // Fire off API call
-            searchMovies(debouncedQuery).then(results => {
-                setMovies(results);
-            });
-            } else {
-                setMovies([]);
-            }
-        },
-        [debouncedQuery]
-  );
+//     useEffect(() => {
+//     // Make sure we have a value (user has entered something in input)
+//         if (debouncedQuery) {
+//             // Fire off API call
+//             searchMovies(debouncedQuery).then(results => {
+//                 setMovies(results);
+//             });
+//             } else {
+//                 setMovies([]);
+//             }
+//         },
+//         [debouncedQuery]
+//   );
 
     // functions
     const signOut = () => {
@@ -102,7 +104,7 @@ const Home = () => {
         setSort("year");
     }
 
-    const selectedSort = (selected) => {
+    const sortStyle = (selected) => {
         if(sort === selected){
             return {
                 color: "#00d38e",
@@ -115,27 +117,30 @@ const Home = () => {
 
     useEffect(() => {
         if(sort === "rating"){
-            setMovies(movies.map((a, b) => {
+            movies.sort((a, b) => {
                 if (a.vote_average > b.vote_average) return -1;
                 if (a.vote_average < b.vote_average) return 1;
                 return 0;
-            }))
+            })
+            setMovies([...movies])
         }
         else if(sort === "year"){
-            setMovies(movies.map((a, b) => {
+            movies.sort((a, b) => {
                 if (a.release_date.substring(0, 4) > b.release_date.substring(0, 4)) return -1;
                 if (a.release_date.substring(0, 4) < b.release_date.substring(0, 4)) return 1;
                 return 0;
-            }))
+            })
+            setMovies([...movies])
         }
         else {
             searchMovies();
         }
     }, [sort])
 
+
     const searchMovies = async () => {
 
-        try{
+        try {
             let res = await axios.get(
                 "https://api.themoviedb.org/3/search/movie?api_key=" +
                 API_KEY +
@@ -148,8 +153,8 @@ const Home = () => {
             setMovies(data.results);
         }
         // Catches API limit errors, reloads page until resets
-        catch(err){
-            if(err.status === "429"){
+        catch (err) {
+            if (err.status === "429") {
                 console.log('Error 429');
                 setError(true);
                 setLoaded(false);
@@ -157,6 +162,7 @@ const Home = () => {
             searchMovies(query);
         }
     }
+
 
     const renderNavbar = () => {
         return (
@@ -169,13 +175,13 @@ const Home = () => {
                     alignItems="center"
                 >
                     <Grid item>
-                    <img className="Navbar-img" src={logo} alt="FilmShark" />
+                    <img className="Navbar-img" src={logo} alt="FilmShark" onClick={reset} />
                     </Grid>
                     <Grid item>
                         <Typography className={classes.root}>
-                            <Link className={classes.button} onClick={sortByRelevance}>Relevance</Link>
-                            <Link className={classes.button} onClick={sortByRating}>Rating</Link>
-                            <Link className={classes.button} onClick={sortByYear}>Year</Link>
+                            <Link className={classes.button} style={sortStyle("relevance")} onClick={sortByRelevance}>Relevance</Link>
+                            <Link className={classes.button} style={sortStyle("rating")} onClick={sortByRating}>Rating</Link>
+                            <Link className={classes.button} style={sortStyle("year")} onClick={sortByYear}>Year</Link>
                         </Typography>
                     </Grid>
                     <Grid item>
@@ -186,6 +192,7 @@ const Home = () => {
                     </Typography>
                     </Grid>
                 </Grid>
+                <TextField />
                 </div>
             </div>
         )
@@ -193,20 +200,37 @@ const Home = () => {
 
     const renderContent = () => {
         if(loaded && movies.length >= 1){
-            
+            return (
+                <div className="Home-content">
+                    {movies.map(movie => (
+                        <MovieCard 
+                        title={movie.title} 
+                        rating={movie.vote_average} 
+                        poster-img={movie.poster_path} 
+                        backdrop-img={movie.backdrop_path} 
+                        description={movie.overview}
+                        release-date={movie.release_date}
+                        id={movie.id}
+                        key={movie.id}
+                        />
+                    ))}
+                </div>
+            )
         }
         else {
-
+            return (
+                <h1>No movies found</h1>
+            )
         }
     }
 
     return (
-        <div className="Home">
-            <div className="Home-header">
+        <div className="Home-app">
+            <div>
                 {renderNavbar()}
             </div>
-            <div>
-                {/* render content */}
+            <div className={classes.container}>
+                {renderContent()}
             </div>
         </div>
     )
