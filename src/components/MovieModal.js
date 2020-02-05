b4de018d310cb15db815f46928a4a9a55a75c02b
Ajschlander <Aschlander21@gmail.com>
2020-02-05 16:44:08 -0600

    New firebase project config

--- src/components/MovieModal.js
+++ src/components/MovieModal.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { IoIosPlayCircle } from "react-icons/io";
+import { IoIosPlayCircle, IoMdEye, IoIosAddCircle } from "react-icons/io";
 import { Grid, Button } from "@material-ui/core";
 import Modal from "react-modal";
 import axios from "axios";
--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,14 @@ import * as firebase from "firebase/app";
 import "firebase/auth";
 
 const app = firebase.initializeApp({
-	apiKey: "AIzaSyAxns8Mk2LKYq8I_kBwjOZiUC-1Y_br0A8",
-	authDomain: "filmshark-react.firebaseapp.com",
-	databaseURL: "https://filmshark-react.firebaseio.com",
-	projectId: "filmshark-react",
-	storageBucket: "filmshark-react.appspot.com",
-	messagingSenderId: "116506343238",
-	appId: "1:116506343238:web:8d3725599b75dc28f85747",
-	measurementId: "G-1RHP93K18M"
+	apiKey: "AIzaSyCj8fo0CdYYSYtzL2Xmhx1DEMgvevogt5g",
+	authDomain: "filmshark-c0324.firebaseapp.com",
+	databaseURL: "https://filmshark-c0324.firebaseio.com",
+	projectId: "filmshark-c0324",
+	storageBucket: "filmshark-c0324.appspot.com",
+	messagingSenderId: "3960362904",
+	appId: "1:3960362904:web:b5b18ba71541985feabded",
+	measurementId: "G-ZCTLKW26JK"
 });
 
 export default app;
