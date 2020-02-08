5d9e7fdd4a371f0ee768f8560ae0c945b495f17b
Ajschlander <Aschlander21@gmail.com>
2020-02-07 22:46:53 -0600

    got rid of client side collection creation

--- src/containers/Signup.js
+++ src/containers/Signup.js
@@ -58,13 +58,7 @@ const SignUp = ({ history }) => {
 					.createUserWithEmailAndPassword(
 						email.value,
 						password.value
-					).then(cred => {
-						console.log(db.collection("users"));
-						db.collection("users").doc(cred.user.uid).set({
-							email: email.value
-						})
-						console.log(db.collection("users"));
-					}).catch(err => {
+					).catch(err => {
 						console.log(err);
 					});
 				history.push("/home");
